525096143691789e0905a0dfbb0df9e90c0843a1
stellamaranhao <109309376+stellamaranhao@users.noreply.github.com>
2023-12-10 14:11:49 -0300

    Funcoes getIndex e getShift funcionando

--- simulador.c
+++ simulador.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <math.h>
 
 #define MAX_MEMORY_SIZE 4 * 1024 * 1024 // 4MB
 #define MAX_PAGE_SIZE 16 * 1024 // 16KB
@@ -17,11 +17,12 @@ void verifica_limites(int page_size, int total_memory) {
     }
 }
 
-void prtint_parameters(char *algorithm, char *log_file, int page_size, int total_memory) {
+void prtint_parameters(char *algorithm, char *log_file, int page_size, int total_memory, int shift) {
     printf("Algoritmo: %s\n", algorithm);
     printf("Arquivo de log: %s\n", log_file);
     printf("Tamanho de página: %d KB\n", page_size / 1024);
     printf("Tamanho de memória: %d MB\n", total_memory / 1024 / 1024);
+    printf("Deslocamento: %d MB\n", shift);
 }
 
 void verifica_arquivo(FILE *file) {
@@ -31,11 +32,24 @@ void verifica_arquivo(FILE *file) {
     }
 }
 
+int getShift(int page_size){
+    int shift;
+    double log2_numero = log2(page_size);
+    shift = (int)round(log2_numero);
+    return shift;
+}
+int getIndex(unsigned int addr, int shift){
+    unsigned int index = addr >> shift;
+
+    return index;
+}
+
 int main(int argc, char *argv[]) {
     if (argc != 5) {
         fprintf(stderr, "Uso: %s <algoritmo> <arq.log> <tamanho_pagina> <tamanho_memoria>\n", argv[0]);
         exit(EXIT_FAILURE);
     }
+
 
     // Argumentos de linha de comando
     char *algorithm = argv[1];
@@ -44,20 +58,18 @@ int main(int argc, char *argv[]) {
     int total_memory = atoi(argv[4]) * 1024 * 1024; // Convertendo para bytes
     FILE **files = malloc(sizeof(FILE *) * 4);
     verifica_limites(page_size, total_memory);
-    
-    prtint_parameters(algorithm, log_file, page_size, total_memory);
+    int shift = getShift(page_size);
+    prtint_parameters(algorithm, log_file, page_size, total_memory, shift);
     FILE *file = fopen(log_file, "r");
     verifica_arquivo(file);
 
     unsigned int addr;
     char rw;
 
-    //Ler os 4 arquivos em paralelo ou sequencial???
     while (fscanf(file, "%x %c", &addr, &rw) == 2) {
-        printf("Address: 0x%x, Read/Write: %c\n", addr, rw);
+        printf("Address: 0x%x, Read/Write: %c, Page index: 0x%x - %u\n", addr, rw, getIndex(addr, shift), getIndex(addr, shift));
     }
 
-    // Close the file
     fclose(file);
 
     return 0;
